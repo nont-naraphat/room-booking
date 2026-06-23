20f833c5d7735bb7bd99ab878020d6fb633cf042
nont-naraphat <it@21sunpassion.com>
2026-06-23 14:16:52 +0700

    Update index.js

--- meeting-room-booking/meeting-room-booking/server/index.js
+++ meeting-room-booking/meeting-room-booking/server/index.js
@@ -36,7 +36,7 @@ app.use(
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 วัน
     httpOnly: true,
     sameSite: "lax",
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.COOKIE_SECURE === "true",
   })
 );
 
